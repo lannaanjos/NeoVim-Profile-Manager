130ee1e10bada435047100c383a60f65a1af2047
Lacci <lannadosanjoscosta@gmail.com>
2026-06-07 20:59:07 -0300

    feat(common keymaps): select all and cut selection

--- core/lua/config/keymaps.lua
+++ core/lua/config/keymaps.lua
@@ -1,13 +1,17 @@
--- configurações de clipboard
-vim.keymap.set("v", "<C-c>", '"+y', { desc = "Copia selecao pro clipboard" })
-vim.keymap.set({ "n", "v" }, "<C-S-c>", '"+y', { desc = "Copia pro clipboard" })
-vim.keymap.set("n", "<C-S-v>", '"+p', { desc = "Cola do clipboard" })
-vim.keymap.set("i", "<C-S-v>", "<C-R>+", { desc = "Cola do clipboard (modo insert)" })
+-- clipboard configuration
+vim.keymap.set("v", "<C-c>", '"+y', { desc = "Copy selection to clipboard Visual" })
+vim.keymap.set({ "n", "v" }, "<C-S-c>", '"+y', { desc = "Copy selection to clipboard Normal Visual" })
+vim.keymap.set("n", "<C-S-v>", '"+p', { desc = "Paste from clipboard Normal" })
+vim.keymap.set("i", "<C-S-v>", "<C-R>+", { desc = "Paste from clipboard Insert" })
+vim.keymap.set("v", "<C-x>", '"+d', { desc = "Cut selection Visual" })
 
--- configurações do editor
-vim.keymap.set("n", "<C-z>", "u", { desc = "desfaz" })
-vim.keymap.set("i", "<C-z>", "<Esc>u", { desc = "desfaz" })
+-- editor configuration
+vim.keymap.set("n", "<C-z>", "u", { desc = "Undo Normal" })
+vim.keymap.set("i", "<C-z>", "<Esc>u", { desc = "Undo Insert" })
 
--- navegação
-vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>", { desc = "neo-tree" }) -- abre e fecha neotree
+-- file navigation
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>", { desc = "Open and close neo-tree" })
 
+-- text navigation
+vim.keymap.set("n", "<C-t>", "ggVG", { desc = "Select all Normal" })
+vim.keymap.set("i", "<C-t>", "<Esc>ggVG", { desc = "Select all Insert" })
